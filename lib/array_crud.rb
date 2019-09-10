718f7c051e35e79288d2006e89f847431d372b80
Keri Deemer <kerid18@att.net>
2019-09-10 19:09:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
+  colors = ["blue", "black", "green", "yellow"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  colors >> ""
 end
 
 def add_element_to_start_of_array(array, element)
